98ac9c362150f819b80fa7f1465f5c241c6a60f8
prl405 <73357564+prl405@users.noreply.github.com>
2021-10-20 22:00:37 +0100

    More Entities and Repositories

---
+++ src/main/java/com/sparta/northwindrest/entities/Employee.java
@@ -0,0 +1,20 @@
+package com.sparta.northwindrest.entities;
+
+import javax.persistence.*;
+
+@Table(name = "employees")
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "EmployeeID", nullable = false)
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
