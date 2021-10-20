a4edc3bbc87acb13f25e841e0e61d93611c84ca3
prl405 <73357564+prl405@users.noreply.github.com>
2021-10-20 20:12:14 +0100

    Wednesday Morning Class code

--- src/main/java/com/sparta/northwindrest/controllers/NorthwindController.java
+++ src/main/java/com/sparta/northwindrest/controllers/NorthwindController.java
@@ -1,12 +1,25 @@
 package com.sparta.northwindrest.controllers;
 
+import com.sparta.northwindrest.entities.ProductEntity;
 import com.sparta.northwindrest.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
 public class NorthwindController {
 
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
+    @Autowired
     public NorthwindController(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products")
+    public List<ProductEntity> getAllProducts(){
+        return productRepository.findAll();
     }
 }
