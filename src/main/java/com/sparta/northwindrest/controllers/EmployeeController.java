ed3339a4db7b94737b72b3ca02e804a245614959
prl405 <73357564+prl405@users.noreply.github.com>
2021-10-21 18:17:22 +0100

    Built out Employee class

--- src/main/java/com/sparta/northwindrest/controllers/EmployeeController.java
+++ src/main/java/com/sparta/northwindrest/controllers/EmployeeController.java
@@ -3,11 +3,12 @@ package com.sparta.northwindrest.controllers;
 import com.sparta.northwindrest.entities.EmployeeEntity;
 import com.sparta.northwindrest.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 @RestController
 public class EmployeeController {
@@ -21,22 +22,51 @@ public class EmployeeController {
 
     @GetMapping("/employees")
     @ResponseBody
-    public List<EmployeeEntity> getAllEmployees(){
-        return employeeRepository.findAll();
+    public List<EmployeeEntity> getEmployees(@RequestParam(required = false) String firstName,
+                                             @RequestParam(required = false) String lastName,
+                                             @RequestParam(required = false) String country,
+                                             @RequestParam(required = false) String city){
+
+        List<EmployeeEntity> foundEntities = new ArrayList<>();
+        for (EmployeeEntity employeeEntity : employeeRepository.findAll()) {
+            if (checkFirstName(employeeEntity, firstName) && !foundEntities.contains(employeeEntity)) {
+                foundEntities.add(employeeEntity);
+            }
+            if (lastName != null && (employeeEntity.getLastName().contains(lastName)
+                    || employeeEntity.getLastName().toLowerCase().contains(lastName))
+                    && !foundEntities.contains(employeeEntity)) {
+                foundEntities.add(employeeEntity);
+            }
+            if (country != null && (employeeEntity.getCountry().contains(country)
+                    || employeeEntity.getCountry().toLowerCase().contains(country))
+                    && !foundEntities.contains(employeeEntity)) {
+                foundEntities.add(employeeEntity);
+            }
+            if (checkCity(employeeEntity, city) && !foundEntities.contains(employeeEntity)) {
+                foundEntities.add(employeeEntity);
+            }
+        }
+        if (foundEntities.size() == 0){
+            return employeeRepository.findAll();
+        }
+        else {
+            return foundEntities;
+        }
     }
 
-//    @GetMapping("/employees")
-//    @ResponseBody
-//    public List<EmployeeEntity> getEmployeesFirstNameAndCountry(@RequestParam(required = false) String firstName,
-//                                                                  @RequestParam(required = false) String country){
-//
-//        if (firstName == null || country == null){
-//            return employeeRepository.findAll();
-//        }
-//        return employeeRepository.findAll()
-//                .stream()
-//                .filter(employeeEntity -> employeeEntity.getFirstName().contains(firstName) &&
-//                        employeeEntity.getCountry().equals(country))
-//                .toList();
-//    }
+    @GetMapping("/employees/{id}")
+    @ResponseBody
+    public Optional<EmployeeEntity> getEmployeeById(@PathVariable Integer id){
+        return employeeRepository.findById(id);
+    }
+
+    private boolean checkCity(EmployeeEntity employeeEntity, String city){
+        return city != null && (employeeEntity.getCity().contains(city)
+                || employeeEntity.getCity().toLowerCase().contains(city));
+    }
+
+    private boolean checkFirstName(EmployeeEntity employeeEntity, String firstName){
+        return firstName != null && (employeeEntity.getFirstName().contains(firstName)
+                || employeeEntity.getFirstName().toLowerCase().contains(firstName));
+    }
 }
