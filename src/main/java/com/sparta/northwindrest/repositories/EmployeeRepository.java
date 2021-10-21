7d9363c297dce9ea73225b0f8f91fb909afecb7a
prl405 <73357564+prl405@users.noreply.github.com>
2021-10-21 13:29:52 +0100

    EmloyeeEntity fixed

--- src/main/java/com/sparta/northwindrest/repositories/EmployeeRepository.java
+++ src/main/java/com/sparta/northwindrest/repositories/EmployeeRepository.java
@@ -1,6 +1,5 @@
 package com.sparta.northwindrest.repositories;
 
-import com.sparta.northwindrest.entities.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer> {
