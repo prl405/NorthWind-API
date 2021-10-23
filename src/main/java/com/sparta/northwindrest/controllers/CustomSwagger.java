a9b38302d41852a8e49643c14d0714a38a7b8e17
prl405 <73357564+prl405@users.noreply.github.com>
2021-10-23 18:31:01 +0100

    Swagger working

--- src/main/java/com/sparta/northwindrest/controllers/CustomSwagger.java
+++ src/main/java/com/sparta/northwindrest/controllers/CustomSwagger.java
@@ -1,4 +1,4 @@
-package customswagger;
+package com.sparta.northwindrest.controllers;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
